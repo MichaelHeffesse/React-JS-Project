c4a928e4d1568107b291d2b2a25171391a443bb8
MichaelHeffesse <heffessem@gmail.com>
2022-05-25 19:02:44 -0300

    NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+
+function NavBar() {
+
+    return (
+
+        <Navbar collapseOnSelect expand="lg" bg="blue" variant="white">
+        <Container>
+        <img src="../img/Logo.png" alt="" />
+        <Navbar.Brand href="#inicio">Digital HikeUP</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+            <Nav.Link href="#acercadenosotros">Acerca de nosotros</Nav.Link>
+            <Nav.Link href="#servicios">¿Que ofrecemos?</Nav.Link>
+            <NavDropdown title="Servicios" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">1er Servicio</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">2do Servicio</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">3er Servicio</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">4to Servicio</NavDropdown.Item>
+            </NavDropdown>
+            </Nav>
+            <Nav>
+            <Nav.Link href="#deets">Contactate con nosotros</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+                Habla con un asesor
+            </Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+        </Container>
+        </Navbar>
+
+    );
+
+}
+
+export {NavBar};
+
